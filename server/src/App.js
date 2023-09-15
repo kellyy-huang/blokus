f8d48fbb97d76a190b3b9160a1f73505fd06bfce
kelly <kyhuang919@gmail.com>
2023-09-15 14:17:38 -0700

    add letters

--- server/src/App.js
+++ server/src/App.js
@@ -1,7 +1,178 @@
 import "./App.css";
+import React, { useState } from "react";
+
+const letterBank = [
+  "A",
+  "A",
+  "A",
+  "A",
+  "A",
+  "A",
+  "A",
+  "A",
+  "A",
+  "A",
+  "A",
+  "A",
+  "A",
+  "B",
+  "B",
+  "B",
+  "C",
+  "C",
+  "C",
+  "D",
+  "D",
+  "D",
+  "D",
+  "D",
+  "D",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "E",
+  "F",
+  "F",
+  "F",
+  "G",
+  "G",
+  "G",
+  "G",
+  "H",
+  "H",
+  "H",
+  "I",
+  "I",
+  "I",
+  "I",
+  "I",
+  "I",
+  "I",
+  "I",
+  "I",
+  "I",
+  "I",
+  "I",
+  "J",
+  "J",
+  "K",
+  "K",
+  "L",
+  "L",
+  "L",
+  "L",
+  "L",
+  "M",
+  "M",
+  "M",
+  "N",
+  "N",
+  "N",
+  "N",
+  "N",
+  "N",
+  "N",
+  "N",
+  "O",
+  "O",
+  "O",
+  "O",
+  "O",
+  "O",
+  "O",
+  "O",
+  "O",
+  "O",
+  "O",
+  "P",
+  "P",
+  "P",
+  "Q",
+  "Q",
+  "R",
+  "R",
+  "R",
+  "R",
+  "R",
+  "R",
+  "R",
+  "R",
+  "R",
+  "S",
+  "S",
+  "S",
+  "S",
+  "S",
+  "S",
+  "T",
+  "T",
+  "T",
+  "T",
+  "T",
+  "T",
+  "T",
+  "T",
+  "T",
+  "U",
+  "U",
+  "U",
+  "U",
+  "U",
+  "U",
+  "V",
+  "V",
+  "V",
+  "W",
+  "W",
+  "W",
+  "X",
+  "X",
+  "Y",
+  "Y",
+  "Y",
+  "Z",
+  "Z",
+];
 
 function App() {
-  return <div className="App">blokus</div>;
+  const shuffled = letterBank.sort(() => Math.random() - 0.5);
+  const [letters, setLetters] = useState(shuffled.slice(0, 13));
+
+  function showLetters() {
+    let letterBank = [];
+    for (const l of letters) {
+      letterBank.push(<div className="letterContainer">{l}</div>);
+    }
+    return letterBank;
+  }
+
+  function newLetter() {
+    const letter = letterBank.pop();
+  }
+
+  return (
+    <div className="App">
+      <h1>fruitgrams</h1>
+      <div>
+        <button onclick={newLetter()}>Get Letters!</button>
+      </div>
+      {showLetters()}
+    </div>
+  );
 }
 
 export default App;
